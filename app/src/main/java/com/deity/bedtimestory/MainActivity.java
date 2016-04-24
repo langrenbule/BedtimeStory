6987aed2b2a822a6d68d81bb44b056f68b054f1a
langrenbule <546024423@qq.com>
2016-04-24 19:57:50 +0800

    (*)广告集成

--- app/src/main/java/com/deity/bedtimestory/MainActivity.java
+++ app/src/main/java/com/deity/bedtimestory/MainActivity.java
@@ -1,8 +1,11 @@
 package com.deity.bedtimestory;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 import com.deity.bedtimestory.adapter.TypeAdapter;
 import com.viewpagerindicator.TabPageIndicator;
@@ -24,5 +27,22 @@ public class MainActivity extends FragmentActivity {
         TypeAdapter typeAdapter = new TypeAdapter(getSupportFragmentManager());
         mContentPage.setAdapter(typeAdapter);
         mIndicator.setViewPager(mContentPage);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("是否退出?")
+                    .setMessage("你要退出应用吗?")
+                    .setNegativeButton("取消",null)
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            System.exit(0);
+                        }
+                    }).show();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
