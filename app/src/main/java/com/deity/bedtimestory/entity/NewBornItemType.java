890caafd2d2dc2f02c394097f60e4f08afbecf36
langrenbule <546024423@qq.com>
2016-11-14 22:55:14 +0800

    强制竖屏 多渠道 包名

--- app/src/main/java/com/deity/bedtimestory/entity/NewBornItemType.java
+++ app/src/main/java/com/deity/bedtimestory/entity/NewBornItemType.java
@@ -7,18 +7,18 @@ package com.deity.bedtimestory.entity;
 
 public enum NewBornItemType {
     十月怀胎(0,"http://www.mamabaobao.com/portal.php?mod=list&catid=104&page="),
-    一朝分娩(1,"http://www.mamabaobao.com/portal.php?mod=list&catid=104&page="),
-    准爸爸读本(2,"http://www.mamabaobao.com/portal.php?mod=list&catid=104&page="),
-    孕前准备(3,"http://www.mamabaobao.com/portal.php?mod=list&catid=104&page="),
-    孕早期(4,"http://www.mamabaobao.com/portal.php?mod=list&catid=104&page="),
-    孕中晚期(5,"http://www.mamabaobao.com/portal.php?mod=list&catid=104&page="),
-    六月宝宝(6,"http://www.mamabaobao.com/portal.php?mod=list&catid=104&page="),
-    周年宝宝(7,"http://www.mamabaobao.com/portal.php?mod=list&catid=104&page="),
-    幼儿心理(8,"http://www.mamabaobao.com/portal.php?mod=list&catid=104&page="),
-    小五神童(9,"http://www.mamabaobao.com/portal.php?mod=list&catid=104&page="),
-    生活照顾(10,"http://www.mamabaobao.com/portal.php?mod=list&catid=104&page="),
-    营养健康(11,"http://www.mamabaobao.com/portal.php?mod=list&catid=104&page="),
-    亲子共读(12,"http://www.mamabaobao.com/portal.php?mod=list&catid=104&page=");
+    一朝分娩(1,"http://www.mamabaobao.com/portal.php?mod=list&catid=105&page="),
+    准爸爸读本(2,"http://www.mamabaobao.com/portal.php?mod=list&catid=106&page="),
+    孕前准备(3,"http://www.mamabaobao.com/portal.php?mod=list&catid=107&page="),
+    孕早期(4,"http://www.mamabaobao.com/portal.php?mod=list&catid=108&page="),
+    孕中晚期(5,"http://www.mamabaobao.com/portal.php?mod=list&catid=109&page="),
+    六月宝宝(6,"http://www.mamabaobao.com/portal.php?mod=list&catid=95&page="),
+    周年宝宝(7,"http://www.mamabaobao.com/portal.php?mod=list&catid=96&page="),
+    幼儿心理(8,"http://www.mamabaobao.com/portal.php?mod=list&catid=97&page="),
+    小五神童(9,"http://www.mamabaobao.com/portal.php?mod=list&catid=99&page="),
+    生活照顾(10,"http://www.mamabaobao.com/portal.php?mod=list&catid=101&page="),
+    营养健康(11,"http://www.mamabaobao.com/portal.php?mod=list&catid=102&page="),
+    亲子共读(12,"http://www.mamabaobao.com/portal.php?mod=list&catid=119&page=");
 
     private int code;
     private String targetUrl;
